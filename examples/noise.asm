ae1e67405373ddc3c7ca2edf5f309950749ff5dd
las-r <128936184+las-r@users.noreply.github.com>
2026-06-30 18:09:30 -0400

    Add noise asm and bin

---
+++ examples/noise.asm
@@ -0,0 +1,32 @@
+; font noise
+; by las-r
+
+PLN 15
+
+; constants
+LDI R1 1
+LDI R6 15
+LDI R7 63
+LDI R8 127
+
+; loop back offset
+; 8 instructions to jump back: (8 * 2) + 2 = 18 bytes
+LDI R9 22     
+
+; main blast loop
+    RNG R2        ; [PC here] 1
+    AND R2 R2 R8   ; 2
+
+    RNG R3        ; 3
+    AND R3 R3 R7   ; 4
+
+    RNG R4        ; 5
+    AND R4 R4 R6   ; 6
+
+    FNT R4        ; 7
+    DRW R2 R3 5   ; 8
+
+    SKU 0xd
+    CLR
+
+JNR R9            ; Loops back to first RNG
