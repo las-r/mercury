372a7c884bac9358f0a4c3709a095c685f24567c
las-r <128936184+las-r@users.noreply.github.com>
2026-06-30 20:01:35 -0400

    remove whitespace

--- examples/streaks.asm
+++ examples/streaks.asm
@@ -8,12 +8,12 @@ LDI R1 1
 LDI R6 15
 LDI R7 63
 LDI R8 127
-LDI R9 8      ; horizontal bar spacing stride
+LDI R9 8 ; horizontal bar spacing stride
 
 ; screen coordinates
-LDI R2 16     ; start x offset
-LDI R3 0      ; start y offset
-LDI R4 13     ; solid character glyph index
+LDI R2 16 ; start x offset
+LDI R3 0 ; start y offset
+LDI R4 13 ; solid character glyph index
 
 ; setup absolute loop target
 LDI Ra waterfall_loop
@@ -25,11 +25,11 @@ waterfall_loop:
     ; move down quickly
     LDI Re 3
     ADD R3 R3 Re
-    AND R3 R3 R7   ; naturally wraps y past 63 back to 0
+    AND R3 R3 R7 ; naturally wraps y past 63 back to 0
 
     ; slide columns right continuously
     ADD R2 R2 R1
-    AND R2 R2 R8   ; naturally wraps x past 127 back to 0
+    AND R2 R2 R8 ; naturally wraps x past 127 back to 0
 
 ; loop back safely
 JMP R0 Ra R0
